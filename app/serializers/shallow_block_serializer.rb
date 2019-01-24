a7faa652c396560e09949bcdfbfe10898b862cf0
github username <github email address>
2019-01-24 17:20:54 -0500

    url change

--- app/serializers/shallow_block_serializer.rb
+++ app/serializers/shallow_block_serializer.rb
@@ -12,11 +12,11 @@ class ShallowBlockSerializer < ActiveModel::Serializer
   def file
       begin
         # FOR RUNNING W JUST ACTIVE RECORD
-        short_url = Rails.application.routes.url_helpers.rails_blob_path(object.file, only_path: true)
-        url = "https://623c5e07.ngrok.io" + short_url
+        # short_url = Rails.application.routes.url_helpers.rails_blob_path(object.file, only_path: true)
+        # url = "https://623c5e07.ngrok.io" + short_url
 
         # FOR RUNNING WITH s3
-        # url = object.file.service_url
+        url = object.file.service_url
         return url
       rescue
       return ''
